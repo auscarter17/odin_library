e3bb5e30f0683fd57224f16f5250a25b7c6c22d7
auscarter17 <auscarter17@gmail.com>
2025-05-15 15:03:39 -0400

    added function for adding new books to library

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-function Book(title, author, pages, read) {
+const myLibrary = []
+
+function Book(id, title, author, pages, read) {
+  this.id = id
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -9,6 +12,13 @@ function Book(title, author, pages, read) {
 }
 
 //theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "Not read yet")
-console.log(theHobbit.info())
-console.log('test')
+//const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "Not read yet")
+
+function addBook(id, title, author, pages, read) {
+  const newBook = new Book (id, title, author, pages, read)
+  myLibrary.push(newBook)
+}
+
+addBook(1,"The Hobbit", "J.R.R. Tolkien", "295 pages", "Not read yet")
+
+console.log(myLibrary)
